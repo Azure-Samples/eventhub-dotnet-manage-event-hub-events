7fa3260c8da06af4b2410a2a05d032af77eeab1a
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2023-08-30 17:08:39 +0800

    Migration Track1 to Track2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.Management.CosmosDB.Fluent.Models;
-using Microsoft.Azure.Management.Eventhub.Fluent;
-using Microsoft.Azure.Management.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions;
-using Microsoft.Azure.Management.Samples.Common;
-using Microsoft.Azure.Management.Storage.Fluent;
-using System;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.CosmosDB;
+using Azure.ResourceManager.CosmosDB.Models;
+using Azure.ResourceManager.EventHubs;
+using Azure.ResourceManager.Monitor;
+using Azure.ResourceManager.Monitor.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Samples.Common;
 
 namespace ManageEventHubEvents
 {
@@ -22,47 +24,71 @@ namespace ManageEventHubEvents
      */
     public class Program
     {
-        public static void RunSample(IAzure azure)
+        private static ResourceIdentifier? _resourceGroupId = null;
+
+        public static async Task RunSample(ArmClient client)
         {
-            Region region = Region.USEast;
-            string rgName = SdkContext.RandomResourceName("rgEvHb", 15);
-            string namespaceName = SdkContext.RandomResourceName("ns", 15);
-            string storageAccountName = SdkContext.RandomResourceName("stg", 15);
+            AzureLocation region = AzureLocation.EastUS;
+            string rgName = Utilities.CreateRandomName("rgEvHb");
+            string namespaceName = Utilities.CreateRandomName("ns");
             string eventHubName = "FirstEventHub";
-            string diagnosticSettingId = null;
+            ResourceIdentifier? diagnosticSettingId = null;
 
             try
             {
+                //============================================================
+                // Create a resource group
+                //
+                var subscription = await client.GetDefaultSubscriptionAsync();
+                var resourceGroupData = new ResourceGroupData(region);
+                var resourceGroup = (await subscription.GetResourceGroups()
+                    .CreateOrUpdateAsync(WaitUntil.Completed, rgName, resourceGroupData)).Value;
+                _resourceGroupId = resourceGroup.Id;
+
                 //=============================================================
                 // Creates a Cosmos DB.
                 //
-                var docDb = azure.CosmosDBAccounts
-                        .Define(namespaceName)
-                            .WithRegion(region)
-                            .WithNewResourceGroup(rgName)
-                            .WithKind(DatabaseAccountKind.MongoDB)
-                            .WithEventualConsistency()
-                            .WithWriteReplication(Region.USWest)
-                            .WithReadReplication(Region.USCentral)
-                        .Create();
+                var locations = new List<CosmosDBAccountLocation>
+                {
+                    new CosmosDBAccountLocation()
+                    {
+                        LocationName = AzureLocation.WestUS,
+                        IsZoneRedundant = false,
+                        FailoverPriority = 0,
+                    },
+                    new CosmosDBAccountLocation()
+                    {
+                        LocationName = AzureLocation.SouthCentralUS,
+                        IsZoneRedundant = false,
+                        FailoverPriority = 1,
+                    }
+                };
+                var cosmosData = new CosmosDBAccountCreateOrUpdateContent(region, locations)
+                {
+                    Kind = CosmosDBAccountKind.MongoDB,
+                    ConsistencyPolicy = new ConsistencyPolicy(DefaultConsistencyLevel.Eventual)
+                    {
+                        MaxIntervalInSeconds = 0,
+                        MaxStalenessPrefix = 0
+                    }
+                };
+                var docDb = (await resourceGroup.GetCosmosDBAccounts()
+                    .CreateOrUpdateAsync(WaitUntil.Completed, namespaceName, cosmosData)).Value;
+                Utilities.Log("Created a DocumentDb instance with name: " + docDb.Data.Name);
 
-                Utilities.Log("Created a DocumentDb instance.");
-                Utilities.Print(docDb);
                 //=============================================================
                 // Creates a Event Hub namespace and an Event Hub in it.
                 //
 
                 Utilities.Log("Creating event hub namespace and event hub");
 
-                var ehNamespace = azure.EventHubNamespaces
-                    .Define(namespaceName)
-                        .WithRegion(region)
-                        .WithExistingResourceGroup(rgName)
-                        .WithNewEventHub(eventHubName)
-                    .Create();
+                var eventHubsNamespaceData = new EventHubsNamespaceData(region);
+                var ehNamespace = (await resourceGroup.GetEventHubsNamespaces()
+                    .CreateOrUpdateAsync(WaitUntil.Completed, namespaceName, eventHubsNamespaceData)).Value;
+                var eventHub = (await ehNamespace.GetEventHubs()
+                    .CreateOrUpdateAsync(WaitUntil.Completed, eventHubName, new EventHubData())).Value;
 
-                Utilities.Log($"Created event hub namespace {ehNamespace.Name} and event hub {eventHubName}");
-                Utilities.Print(ehNamespace);
+                Utilities.Log($"Created event hub namespace {ehNamespace.Data.Name} and event hub {eventHub.Data.Name}");
 
                 //=============================================================
                 // Retrieve the root namespace authorization rule.
@@ -70,9 +96,8 @@ namespace ManageEventHubEvents
 
                 Utilities.Log("Retrieving the namespace authorization rule");
 
-                var eventHubAuthRule = azure.EventHubNamespaces
-                    .AuthorizationRules
-                    .GetByName(ehNamespace.ResourceGroupName, ehNamespace.Name, "RootManageSharedAccessKey");
+                var eventHubAuthRule = (await ehNamespace.GetEventHubsNamespaceAuthorizationRules()
+                    .GetAsync("RootManageSharedAccessKey")).Value;
 
                 Utilities.Log("Namespace authorization rule Retrieved");
 
@@ -83,16 +108,35 @@ namespace ManageEventHubEvents
                 Utilities.Log("Enabling diagnostics events of a cosmosdb to stream to event hub");
 
                 // Store Id of created Diagnostic settings only for clean-up
-                var ds = azure.DiagnosticSettings
-                        .Define("DiaEventHub")
-                            .WithResource(docDb.Id)
-                            .WithEventHub(eventHubAuthRule.Id, eventHubName)
-                            .WithLog("DataPlaneRequests", 0)
-                            .WithLog("MongoRequests", 0)
-                            .WithMetric("AllMetrics", TimeSpan.FromMinutes(5), 0)
-                        .Create();
-
-                Utilities.Print(ds);
+                var diagnosticSettingData = new DiagnosticSettingData()
+                {
+                    Metrics =
+                    {
+                        new MetricSettings(true)
+                        {
+                            Category = "AllMetrics",
+                            TimeGrain = TimeSpan.FromMinutes(5),
+                            RetentionPolicy = new RetentionPolicy(false, 0)
+                        }
+                    },
+                    EventHubName = eventHubName,
+                    EventHubAuthorizationRuleId = eventHubAuthRule.Id,
+                    Logs =
+                    {
+                        new LogSettings(true)
+                        {
+                            Category = "DataPlaneRequests",
+                            RetentionPolicy = new RetentionPolicy(false, 0)
+                        },
+                        new LogSettings(true)
+                        {
+                            Category = "MongoRequests",
+                            RetentionPolicy = new RetentionPolicy(false, 0)
+                        }
+                    }
+                };
+                var ds = (await client.GetDiagnosticSettings(docDb.Id)
+                    .CreateOrUpdateAsync(WaitUntil.Completed, "DiaEventHub", diagnosticSettingData)).Value;
                 diagnosticSettingId = ds.Id;
 
                 Utilities.Log("Streaming of diagnostics events to event hub is enabled");
@@ -107,9 +151,15 @@ namespace ManageEventHubEvents
                     if (diagnosticSettingId != null)
                     {
                         Utilities.Log("Deleting Diagnostic Setting: " + diagnosticSettingId);
-                        azure.DiagnosticSettings.DeleteById(diagnosticSettingId);
+                        await client.GetDiagnosticSettingResource(diagnosticSettingId).DeleteAsync(WaitUntil.Completed);
+                        Console.WriteLine($"Deleted Diagnostic Setting: {diagnosticSettingId}");
                     }
-                    azure.ResourceGroups.DeleteByName(rgName);
+                    if (_resourceGroupId is not null)
+                    {
+                        Console.WriteLine($"Deleting Resource Group: {_resourceGroupId}");
+                        await client.GetResourceGroupResource(_resourceGroupId).DeleteAsync(WaitUntil.Completed);
+                        Console.WriteLine($"Deleted Resource Group: {_resourceGroupId}");
+                    }
                 }
                 catch (NullReferenceException)
                 {
@@ -122,24 +172,19 @@ namespace ManageEventHubEvents
             }
         }
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             try
             {
                 //=================================================================
                 // Authenticate
-                var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+                var credential = new DefaultAzureCredential();
 
-                var azure = Azure
-                    .Configure()
-                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
-                    .Authenticate(credentials)
-                    .WithDefaultSubscription();
+                var subscriptionId = Environment.GetEnvironmentVariable("AZURE_SUBSCRIPTION_ID");
+                // you can also use `new ArmClient(credential)` here, and the default subscription will be the first subscription in your list of subscription
+                var client = new ArmClient(credential, subscriptionId);
 
-                // Print selected subscription
-                Utilities.Log("Selected subscription: " + azure.SubscriptionId);
-
-                RunSample(azure);
+                await RunSample(client);
             }
             catch (Exception ex)
             {
